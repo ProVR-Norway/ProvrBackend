83c51987dadc2f94f14c5ab54575a3c859ef39a5
oysteinlondal <oystein@nenett.no>
2021-03-10 15:56:58 +0100

    Added the necessary files.

---
+++ local-setup.sh
@@ -0,0 +1,19 @@
+echo "Executing"
+
+npm install
+
+# Initialize docker image for server
+
+echo "Initializing image and running container for server"
+
+docker build -t ProvrAuthMicroservice .
+
+docker run -p 49100:8080 -d ProvrAuthMicroservice
+
+echo "Initialize image and running container for MySQL database"
+
+docker-compose up
+
+
+
+
