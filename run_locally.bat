140549ed751a1ee0f656e6d69f0c31f9f4768a85
olehjelle <69794502+olehjelle@users.noreply.github.com>
2021-03-11 18:09:35 +0100

    Added auth-microservice in docker-compose.yml, removed starting server from batch file.

--- run_locally.bat
+++ run_locally.bat
@@ -1,12 +1,12 @@
 @echo off
 
-call npm install
+REM call npm install
 
-echo "Building server-dockerimage"
-docker build -t provr-auth-microservice .
+REM  "Building server-dockerimage"
+REM docker build -t provr-auth-microservice .
 
-echo "Starting server-dockerimage"
-docker run -p 49100:8080 -d --name auth-microservice provr-auth-microservice
+REM echo "Starting server-dockerimage"
+REM docker run -p 49100:8080 -d --name auth-microservice provr-auth-microservice
 
 echo "Composing MySQL database from the docker-compose.yml"
 docker-compose up
