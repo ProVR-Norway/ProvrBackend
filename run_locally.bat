66474fe3c7130743251df557818f5fc834bb8bbf
oysteinlondal <oystein@nenett.no>
2021-03-11 11:31:04 +0100

    Update run_locally.bat

--- run_locally.bat
+++ run_locally.bat
@@ -6,7 +6,7 @@ echo "Building server-dockerimage"
 docker build -t provr-auth-microservice .
 
 echo "Starting server-dockerimage"
-docker run -p 49100:8080 -d provr-auth-microservice
+docker run -p 49100:8080 -d --name auth-microservice provr-auth-microservice
 
 echo "Composing MySQL database from the docker-compose.yml"
 docker-compose up
