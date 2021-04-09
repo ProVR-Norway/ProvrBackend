9723d14519b4365257e6d7ca26174c7ea55fea71
oysteinlondal <oystein@nenett.no>
2021-04-10 00:15:51 +0200

    Using createTcpPool to connect to Cloud SQL instead of createConnection.

--- auth-microservice/server/server.js
+++ auth-microservice/server/server.js
@@ -7,10 +7,10 @@ const PORT = 8090;
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 
-const login = require('./src/node_modules/handlers/login.js');
-const register = require('./src/node_modules/handlers/register.js');
-const auth_check = require('./src/node_modules/handlers/auth_check.js.js');
-const forgotten_password = require('./src/node_modules/handlers/forgotten_password.js.js');
+const login = require('./handlers/login.js');
+const register = require('./handlers/register.js');
+const auth_check = require('./handlers/auth_check.js.js');
+const forgotten_password = require('./handlers/forgotten_password.js.js');
 
 app.use('/auth/login', login);
 app.use('/auth/register', register);
