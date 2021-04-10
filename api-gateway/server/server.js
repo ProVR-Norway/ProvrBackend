f4ce2d8b0315e6aab83823148028cf46e3fbe16f
oysteinlondal <oystein@nenett.no>
2021-04-10 21:52:32 +0200

    Implemented the API gateway.
    It is compatible with the service-to-service authentication as described by Burak in his message in Slack (09.04.21). It is not yet tested with a private auth-microservice cloud run instance.

--- api-gateway/server/server.js
+++ api-gateway/server/server.js
@@ -1,9 +1,55 @@
-const gateway = require('fast-gateway')
-const server = gateway({
-  routes: [{
-    prefix: '/auth',
-    target: 'https://auth-microservice-s6rss6nenq-lz.a.run.app'
-  }]
-})
+const express = require('express');
+const request = require('request-promise');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-server.start(8080)
+const app = express();
+
+const authApiServiceURL = process.env.URL_AUTH_MICROSERVICE; //https://auth-microservice-s6rss6nenq-lz.a.run.app
+
+// Set up metadata server request
+// See https://cloud.google.com/compute/docs/instances/verifying-instance-identity#request_signature
+const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=';
+
+app.use('/auth', createProxyMiddleware({
+    target: authApiServiceURL,
+    onProxyReq: function (proxyReq, req, res) {
+
+        const tokenRequestOptions = {
+            uri: metadataServerTokenURL + authApiServiceURL,
+            headers: {
+                'Metadata-Flavor': 'Google'
+            }
+        };
+        // Fetch the token, then provide the token in the request to the receiving service
+        request(tokenRequestOptions)
+        .then((token) => {
+            proxyReq.setHeader('Authorization: ', 'Bearer ' + token);
+        })
+        .then((response) => {
+            res.status(200).send(response);
+        })
+        .catch((error) => {
+            res.status(400).send(error);
+        });
+    }
+}));
+
+/*
+app.use('/login', createProxyMiddleware({
+    changeOrigin: true,
+    target:'https://auth-microservice-s6rss6nenq-lz.a.run.app'
+}));
+*/
+/*
+const http      = require('http');
+const httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({
+    changeOrigin: true,
+    target:'https://auth-microservice-s6rss6nenq-lz.a.run.app'
+}).listen(8000); // See (†)
+*/
+
+app.listen(8080);
--- auth-microservice/server/server.js
+++ auth-microservice/server/server.js
@@ -12,10 +12,10 @@ const register = require('./handlers/register.js');
 const auth_check = require('./handlers/auth_check.js');
 const forgotten_password = require('./handlers/forgotten_password.js');
 
-app.use('/login', login);
-app.use('/register', register);
-app.use('/auth_check', auth_check);
-app.use('/forgotten_password', forgotten_password);
+app.use('/auth/login', login);
+app.use('/auth/register', register);
+app.use('/auth/auth_check', auth_check);
+app.use('/auth/forgotten_password', forgotten_password);
 
 app.listen(PORT);
 // console.log(`Running on http://${HOST}:${PORT}`);
