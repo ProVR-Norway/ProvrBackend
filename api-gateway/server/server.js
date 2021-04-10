b72ad69599083cf5c063b762a8990a1335ea7fac
oysteinlondal <oystein@nenett.no>
2021-04-11 00:26:26 +0200

    debugging

--- api-gateway/server/server.js
+++ api-gateway/server/server.js
@@ -10,7 +10,7 @@ const authApiServiceURL = process.env.URL_AUTH_MICROSERVICE; //https://auth-micr
 // See https://cloud.google.com/compute/docs/instances/verifying-instance-identity#request_signature
 const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=';
 
-//var auth_token;
+var auth_token;
 
 app.use('/auth', async (req, res, next) => {
     console.log("Proxy to fetch token.");
@@ -24,7 +24,7 @@ app.use('/auth', async (req, res, next) => {
     await request(tokenRequestOptions)
     .then((token) => {
         console.log("Fetched token: " + token);
-        req.session.token = token;
+        auth_token = token;
     })
     .then((response) => {
         res.status(200).send(response);
@@ -39,8 +39,7 @@ app.use('/auth', createProxyMiddleware({
     target: authApiServiceURL,
     onProxyReq: function (proxyReq, req, res) {
         console.log("onProxyReq.");
-        var token = req.session.token;
-        proxyReq.setHeader('Authorization: ', 'Bearer ' + token);
+        proxyReq.setHeader('Authorization: ', 'Bearer ' + auth_token);
         //console.log(auth_token);
         //proxyReq.setHeader('Authorization: ', 'Bearer ' + auth_token);
     }
