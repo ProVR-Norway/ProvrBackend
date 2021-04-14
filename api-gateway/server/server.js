83576eaa2c13444dd6d611d196811de536e97aee
oysteinlondal <oystein@nenett.no>
2021-04-14 20:59:37 +0200

    Debugging

--- api-gateway/server/server.js
+++ api-gateway/server/server.js
@@ -15,6 +15,8 @@ const authApiServiceURL = 'https://auth-microservice-development-iu3tuzfidq-ez.a
 // See https://cloud.google.com/compute/docs/instances/verifying-instance-identity#request_signature
 const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=';
 
+/*
+
 var app = express();
 
 app.use('/auth/**', async (req, res, next) => {
@@ -45,6 +47,8 @@ app.use('/auth/**', async (req, res, next) => {
     next()
 })
 
+*/
+
 var options = {
     target: authApiServiceURL,
     //changeOrigin: true,
@@ -63,7 +67,7 @@ var options = {
         );
     },
     // onProxyReq must be below OnProxyRes and OnError!
-    onProxyReq: async (proxyReq, req, res) => {
+    onProxyReq: function (proxyReq, req, res) {
         console.log("onProxyReq.");
         console.log(req.body);
         console.log("Second handler header: " + req.headers);
@@ -71,7 +75,8 @@ var options = {
         console.log("Session token: " + res.locals.token);
         //proxyReq.setHeader('Authorization', 'Bearer ' + auth_token);
         //req.headers['Authorization'] = 'Bearer ' + res.locals.token;
-        await proxyReq.setHeader('Authorization','Bearer ' + res.locals.token);
+        let token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3NDU3MzIxOGM2ZjZhMmZlNTBlMjlhY2JjNjg2NDMyODYzZmM5YzMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2F1dGgtbWljcm9zZXJ2aWNlLWRldmVsb3BtZW50LWl1M3R1emZpZHEtZXouYS5ydW4uYXBwL2F1dGgvbG9naW4iLCJhenAiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUiLCJlbWFpbCI6IjEyMjkxOTk3MDMwMi1jb21wdXRlQGRldmVsb3Blci5nc2VydmljZWFjY291bnQuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTYxODQyOTgwMSwiaWF0IjoxNjE4NDI2MjAxLCJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJzdWIiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUifQ.m6lT5W70NRvqG9wzPaPY3nx6sxSEIMRAkQpDqHion3z1BS7MrNOEXXm6hxRQsI3TAmhIzzJ_IVXxoa6FbJLo5aWLLEZhgyIKAO98pjgkwHRU0iTI5xqz-FNeaT8OsY8DvtJDzn6XTKffE7kcf3L_qjmb8g42ZHEgPzF8bf7QHpciPmpkEZDMARNqV1p56_sA2HyDgAFyIcVKRbs3d15gWFmLOfS5lAlp2ueXIq_gg0eQteDtXZ5GlsX21WJzI-uUJy2gIxyW-eBW0UR5XmZ76SycRzVbSiQYjq4n5iVWXZ89RFqgwz75HW3YldoAMIQhthtPwcv9OzIq19MTORTMMQ';
+        proxyReq.setHeader('Authorization','Bearer ' + token);//res.locals.token);
         //proxyReq.headers['Authorization'] = 'Bearer ' + res.locals.token;
         //req.setHeader('Authorization','Bearer ' + res.locals.token);
         //proxyReq.end()
