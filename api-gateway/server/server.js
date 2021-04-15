311df148d12c655a81f8f8758b370da5f669cd8f
oysteinlondal <oystein@nenett.no>
2021-04-15 14:14:11 +0200

    Back to the original layout with two middleware with the same route
    It is done to see if I can fix this issue.
    
    {
      "name": "StatusCodeError",
      "statusCode": 404,
      "message": "404 - \"<!DOCTYPE html>\\n<html lang=\\\"en\\\">\\n<head>\\n<meta charset=\\\"utf-8\\\">\\n<title>Error</title>\\n</head>\\n<body>\\n<pre>Cannot GET /</pre>\\n</body>\\n</html>\\n\"",
      "error": "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"utf-8\">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET /</pre>\n</body>\n</html>\n",
      "options": {
        "uri": "https://auth-microservice-development-iu3tuzfidq-ez.a.run.app",
        "simple": true,
        "resolveWithFullResponse": false,
        "transform2xxOnly": false
      },
      "response": {
        "statusCode": 404,
        "body": "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"utf-8\">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET /</pre>\n</body>\n</html>\n",
        "headers": {
          "x-powered-by": "Express",
          "content-security-policy": "default-src 'none'",
          "x-content-type-options": "nosniff",
          "content-type": "text/html; charset=utf-8",
          "x-cloud-trace-context": "9f49bd3e684124545061a943b2a1e7fb",
          "date": "Thu, 15 Apr 2021 12:06:43 GMT",
          "server": "Google Frontend",
          "content-length": "139",
          "alt-svc": "h3-29=\":443\"; ma=2592000,h3-T051=\":443\"; ma=2592000,h3-Q050=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000,quic=\":443\"; ma=2592000; v=\"46,43\"",
          "connection": "close"
        },
        "request": {
          "uri": {
            "protocol": "https:",
            "slashes": true,
            "auth": null,
            "host": "auth-microservice-development-iu3tuzfidq-ez.a.run.app",
            "port": 443,
            "hostname": "auth-microservice-development-iu3tuzfidq-ez.a.run.app",
            "hash": null,
            "search": null,
            "query": null,
            "pathname": "/",
            "path": "/",
            "href": "https://auth-microservice-development-iu3tuzfidq-ez.a.run.app/"
          },
          "method": "GET",
          "headers": {
            "authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3NDU3MzIxOGM2ZjZhMmZlNTBlMjlhY2JjNjg2NDMyODYzZmM5YzMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2F1dGgtbWljcm9zZXJ2aWNlLWRldmVsb3BtZW50LWl1M3R1emZpZHEtZXouYS5ydW4uYXBwL2F1dGgvbG9naW4iLCJhenAiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUiLCJlbWFpbCI6IjEyMjkxOTk3MDMwMi1jb21wdXRlQGRldmVsb3Blci5nc2VydmljZWFjY291bnQuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTYxODQ5MjAwMywiaWF0IjoxNjE4NDg4NDAzLCJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJzdWIiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUifQ.JVKdWafSMbbV71fijCIhJNJhntNlhEwZJeQ-TU4-o5MCVyoVtxTM5mVt8V1OEWD3jdzZmvOOTUwy_nEiX7NrGph-s0tND_7Q9RXmVUDUpspoRLCUxHzHoRegk_XSl04jlqv4b20Ym6K_DS-KP-LGnmEX_JbYcrevE_T6CIawLzsE-hgQ1OfUc_kFnAKza3wI63T6LwLab4BRseJdpxPAdfy5zv9h7mhYLnQOT1vVKwZ2iBnduDjQPoxLWalK3qiklFgB3oVMmbGR9etwK_Flk0Z38nkBPBVPr1DZg7D78ShxffcAGP1QTB5TbObzcAsJ_Agu5274TvdDY7Qzn-_1Iw"
          }
        }
      }
    }

--- api-gateway/server/server.js
+++ api-gateway/server/server.js
@@ -17,7 +17,7 @@ const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/serv
 
 var app = express();
 
-/*
+
 app.use('/auth/**', async (req, res, next) => {
     console.log("Proxy to fetch token.");
     console.log("First handler body: " + JSON.stringify(req.body));
@@ -33,7 +33,7 @@ app.use('/auth/**', async (req, res, next) => {
     console.log(metadataServerTokenURL + fullPath);
     // Fetch the token, then provide the token in the request to the receiving service
     //res.locals.token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3NDU3MzIxOGM2ZjZhMmZlNTBlMjlhY2JjNjg2NDMyODYzZmM5YzMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2F1dGgtbWljcm9zZXJ2aWNlLWRldmVsb3BtZW50LWl1M3R1emZpZHEtZXouYS5ydW4uYXBwL2F1dGgvbG9naW4iLCJhenAiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUiLCJlbWFpbCI6IjEyMjkxOTk3MDMwMi1jb21wdXRlQGRldmVsb3Blci5nc2VydmljZWFjY291bnQuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTYxODQyOTgwMSwiaWF0IjoxNjE4NDI2MjAxLCJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJzdWIiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUifQ.m6lT5W70NRvqG9wzPaPY3nx6sxSEIMRAkQpDqHion3z1BS7MrNOEXXm6hxRQsI3TAmhIzzJ_IVXxoa6FbJLo5aWLLEZhgyIKAO98pjgkwHRU0iTI5xqz-FNeaT8OsY8DvtJDzn6XTKffE7kcf3L_qjmb8g42ZHEgPzF8bf7QHpciPmpkEZDMARNqV1p56_sA2HyDgAFyIcVKRbs3d15gWFmLOfS5lAlp2ueXIq_gg0eQteDtXZ5GlsX21WJzI-uUJy2gIxyW-eBW0UR5XmZ76SycRzVbSiQYjq4n5iVWXZ89RFqgwz75HW3YldoAMIQhthtPwcv9OzIq19MTORTMMQ';
-    /*
+    
     // THIS IS THE CAUSE OF THE PROBLEM!!!
     // Can this be done in a different way to see if the problem is fixed then?
     await request(tokenRequestOptions)
@@ -41,15 +41,15 @@ app.use('/auth/**', async (req, res, next) => {
         console.log("Fetched token: " + token);
         res.locals.token = token;
     })
-    .then((response) => {
-        res.status(200).send(response);
-    })
+    //.then((response) => {
+    //    res.status(200).send(response);
+    //})
     .catch((error) => {
         res.status(400).send(error);
     });
     next()
 });
-*/
+
 
 var options = {
     target: authApiServiceURL,
@@ -66,45 +66,26 @@ var options = {
         );
     },
     // onProxyReq must be below OnProxyRes and OnError!
-    onProxyReq: async (proxyReq, req, res) => {
+    onProxyReq: function (proxyReq, req, res) {
         console.log("onProxyReq.");
         console.log(req.body);
         console.log("Second handler header: " + req.headers);
         //const fetched_token = req.headers['Authorization'];
-        //let bearer_token;
-        let fullPath = authApiServiceURL + req.originalUrl;
-        const tokenRequestOptions = {
-            uri: metadataServerTokenURL + fullPath,
-            headers: {
-                'Metadata-Flavor': 'Google'
-            }
-        };
-        await request(tokenRequestOptions)
-        .then((token) => {
-            return request(authApiServiceURL).auth(null, null, true, token)
-            //console.log("Fetched token: " + token);
-            //proxyReq.setHeader('Authorization','Bearer ' + token);
-        })
-        //.then((response) => {
-        //    res.status(201).send(response);
-        //})
-        .catch((error) => {
-            res.status(500).send(error);
-        });
-        //console.log("Session token: " + bearer_token);
+        console.log("Session token: " + res.locals.token);
         //proxyReq.setHeader('Authorization', 'Bearer ' + auth_token);
         //req.headers['Authorization'] = 'Bearer ' + res.locals.token;
-        //let token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3NDU3MzIxOGM2ZjZhMmZlNTBlMjlhY2JjNjg2NDMyODYzZmM5YzMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2F1dGgtbWljcm9zZXJ2aWNlLWRldmVsb3BtZW50LWl1M3R1emZpZHEtZXouYS5ydW4uYXBwL2F1dGgvbG9naW4iLCJhenAiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUiLCJlbWFpbCI6IjEyMjkxOTk3MDMwMi1jb21wdXRlQGRldmVsb3Blci5nc2VydmljZWFjY291bnQuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTYxODQyOTgwMSwiaWF0IjoxNjE4NDI2MjAxLCJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJzdWIiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUifQ.m6lT5W70NRvqG9wzPaPY3nx6sxSEIMRAkQpDqHion3z1BS7MrNOEXXm6hxRQsI3TAmhIzzJ_IVXxoa6FbJLo5aWLLEZhgyIKAO98pjgkwHRU0iTI5xqz-FNeaT8OsY8DvtJDzn6XTKffE7kcf3L_qjmb8g42ZHEgPzF8bf7QHpciPmpkEZDMARNqV1p56_sA2HyDgAFyIcVKRbs3d15gWFmLOfS5lAlp2ueXIq_gg0eQteDtXZ5GlsX21WJzI-uUJy2gIxyW-eBW0UR5XmZ76SycRzVbSiQYjq4n5iVWXZ89RFqgwz75HW3YldoAMIQhthtPwcv9OzIq19MTORTMMQ';//res.locals.token);
+        //let token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc3NDU3MzIxOGM2ZjZhMmZlNTBlMjlhY2JjNjg2NDMyODYzZmM5YzMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJodHRwczovL2F1dGgtbWljcm9zZXJ2aWNlLWRldmVsb3BtZW50LWl1M3R1emZpZHEtZXouYS5ydW4uYXBwL2F1dGgvbG9naW4iLCJhenAiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUiLCJlbWFpbCI6IjEyMjkxOTk3MDMwMi1jb21wdXRlQGRldmVsb3Blci5nc2VydmljZWFjY291bnQuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTYxODQyOTgwMSwiaWF0IjoxNjE4NDI2MjAxLCJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJzdWIiOiIxMTE0MTk0OTg4MDMzOTQ2NzY2NDUifQ.m6lT5W70NRvqG9wzPaPY3nx6sxSEIMRAkQpDqHion3z1BS7MrNOEXXm6hxRQsI3TAmhIzzJ_IVXxoa6FbJLo5aWLLEZhgyIKAO98pjgkwHRU0iTI5xqz-FNeaT8OsY8DvtJDzn6XTKffE7kcf3L_qjmb8g42ZHEgPzF8bf7QHpciPmpkEZDMARNqV1p56_sA2HyDgAFyIcVKRbs3d15gWFmLOfS5lAlp2ueXIq_gg0eQteDtXZ5GlsX21WJzI-uUJy2gIxyW-eBW0UR5XmZ76SycRzVbSiQYjq4n5iVWXZ89RFqgwz75HW3YldoAMIQhthtPwcv9OzIq19MTORTMMQ';
+        proxyReq.setHeader('Authorization','Bearer ' + res.locals.token);//res.locals.token);
         //proxyReq.headers['Authorization'] = 'Bearer ' + res.locals.token;
         //req.setHeader('Authorization','Bearer ' + res.locals.token);
         //proxyReq.end()
         //proxyReq.headers = req.headers;
-        //if(req.body) {
+        if(req.body) {
             //let bodyData = JSON.stringify(req.body);
             // In case if content-type is application/x-www-form-urlencoded -> we need to change to application/json
             // Stream the content
             
-        //}
+        }
         //proxyReq.body = req.body;
         //proxyReq.headers['Content-Type'] = 'text/plain';
         //proxyReq.headers['Authorization'] = 'Bearer ' + res.locals.token;
@@ -114,6 +95,7 @@ var options = {
         //proxyReq.end();
         //console.log(auth_token);
         //proxyReq.setHeader('Authorization: ', 'Bearer ' + auth_token);
+        res.end()
     }
     
 };
