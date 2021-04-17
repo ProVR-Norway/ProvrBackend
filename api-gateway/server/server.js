dd36d1365b8bc8fda2a4d37e181ce2009e12b923
oysteinlondal <oystein@nenett.no>
2021-04-18 00:32:40 +0200

    The first middleware is now in a async function instead.

--- api-gateway/server/server.js
+++ api-gateway/server/server.js
@@ -12,9 +12,11 @@
     handle async functions better. There might just be a problem in the way we are thinking.
     We will need to look into this later when if we get time.
 
+    UPDATE APRIL 17 2021:
+    The issue of concern was tried to be solved, but it seems that when
+
 */
 
-/*
 const express = require('express'); 
 // Needed to send requests to the
 const request = require('request-promise');
@@ -24,6 +26,7 @@ const request = require('request-promise');
 //app.use(express.json()); 
 
 const authApiServiceURL = process.env.URL_AUTH_MICROSERVICE;
+//const cadApiServiceURL = process.env.URL_CAD_MICROSERVICE;
 
 // Set up metadata server request
 // See https://cloud.google.com/compute/docs/instances/verifying-instance-identity#request_signature
@@ -31,7 +34,7 @@ const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/serv
 
 const app = express();
 
-app.use('/**', async (req, res, next) => {
+async function getToken (req, res) {
     // The full path is retrieved based on the following answer:
     // Link: https://stackoverflow.com/a/10185427
     // Set the options for the request to get the token
@@ -45,15 +48,16 @@ app.use('/**', async (req, res, next) => {
     .then((token) => {
         //console.log("Fetched token: " + token);
         //Passing token to the second middleware
-        res.locals.token = token;
+        return token;
     })
     .catch((error) => {
         res.status(400).send(error);
+        return;
     });
-    next()
-});
+};
 
-var options = {
+/*
+var authOptions = {
     target: authApiServiceURL,
     // THE FOLLOWING OPTION NEEDS TO BE HERE EVEN WHEN IT IS UPLOADED TO CLOUD RUN. 
     // IF NOT THE PROXY WON'T WORK PROPERLY!
@@ -76,21 +80,49 @@ var options = {
         //proxyReq.headers['Authorization'] = 'Bearer ' + res.locals.token;
     }
 };
+*/
+
+var authOptions = {
+    target: authApiServiceURL,
+    // THE FOLLOWING OPTION NEEDS TO BE HERE EVEN WHEN IT IS UPLOADED TO CLOUD RUN. 
+    // IF NOT THE PROXY WON'T WORK PROPERLY!
+    changeOrigin: true,
+    onError: function(err, req, res) {
+        res.writeHead(500, {
+          'Content-Type': 'text/plain'
+        });
+        res.end(
+          'The gateway is currently unable to communicated with the requested service.'
+        );
+    },
+    // IMPORTANT! 
+    // The onProxyReq must be below the other events (onError and onProxyRes)
+    // If not the proxyReq will be undefined and we cannot use the setHeader function
+    // The ALTERNATIVE can be used instead
+    onProxyReq: function (proxyReq, req, res) {
+        let id_token = await getToken(req, res);
+        proxyReq.setHeader('Authorization','Bearer ' + id_token);
+        // ALTERNATIVE:
+        //proxyReq.headers['Authorization'] = 'Bearer ' + res.locals.token;
+    }
+};
 
 var { createProxyMiddleware } = require('http-proxy-middleware');
 
 // LISTENS FOR REQUESTS WITH PATH STARTING WITH /auth
 // FOR EXAMPLE auth/login AND auth/register
 // ONLY /auth WILL NOT WORK DUE TO "**"
-app.use('/**', createProxyMiddleware(options));
+app.use('/auth/**', createProxyMiddleware(authOptions));
+//app.use('cadmodels/**', createProxyMiddleware(cadOptions));
 
 // THE PORT MUST BE 8080 WHEN UPLODADED TO CLOUD RUN
 app.listen(8080);
-*/
 
+
+/*
 const express = require('express'); 
 // Needed to send requests to the
-const request = require('request-promise');
+const got = require('got');
 
 // THE FOLLOWING LINE CANNOT BE USED TOGETHER WITH HTTP-PROXY-MIDDLEWARE
 // MORE INFO HERE: https://stackoverflow.com/questions/52270848/zero-response-through-http-proxy-middleware
@@ -124,14 +156,22 @@ var options = {
     onProxyReq: function (proxyReq, req, res) {
         // The full path is retrieved based on the following answer:
         // Link: https://stackoverflow.com/a/10185427
-        // Set the options for the request to get the token
-        const tokenRequestOptions = {
-            uri: metadataServerTokenURL + authApiServiceURL + req.originalUrl,
-            headers: {
-                'Metadata-Flavor': 'Google'
+        (async () => {
+            try {
+                // Set the options for the request to get the token
+                const tokenRequestOptions = {
+                    uri: metadataServerTokenURL + authApiServiceURL + req.originalUrl,
+                    headers: {
+                        'Metadata-Flavor': 'Google'
+                    }
+                };
+                const response = await got(tokenRequestOptions);
+            } catch (error) {
+                console.log(error.response.body);
+                //=> 'Internal server error ...'
             }
-        };
-        request(tokenRequestOptions)
+        })();
+        request()
         .then((token) => {
             //console.log("Fetched token: " + token);
             //Passing token to the second middleware
@@ -154,3 +194,5 @@ app.use('/auth/**', createProxyMiddleware(options));
 
 // THE PORT MUST BE 8080 WHEN UPLODADED TO CLOUD RUN
 app.listen(8080);
+
+*/
