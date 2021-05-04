20a7948e874c8fb66e0430e20332a99c936056ea
oysteinlondal <oystein@nenett.no>
2021-05-04 19:21:49 +0200

    Removed old sql-file

--- userDB.sql
+++
@@ -1,9 +0,0 @@
--- ****************** SqlDBM: MySQL ******************;
--- ***************************************************;
-
-
--- ************************************** `User`
-
-CREATE TABLE User ( userID int AUTO_INCREMENT, username varchar(45) NOT NULL, userEmail varchar(45) NOT NULL, password varchar(32) NOT NULL, PRIMARY KEY (userID));
-
--- https://app.sqldbm.com/MySQL/Edit/p156955/
