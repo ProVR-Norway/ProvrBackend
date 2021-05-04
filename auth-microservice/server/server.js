3d6ac1b7c6516215ce9bc657c202e4311ebbe4c8
oysteinlondal <oystein@nenett.no>
2021-05-04 14:49:19 +0200

    Added rate limit to login requests

--- auth-microservice/server/server.js
+++ auth-microservice/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
 
 /*
 
@@ -17,13 +18,19 @@ app.use(express.json({ limit:'50mb' }));
 // WE NEED THIS SO THAT WE CAN PARSE HTTP REQUESTS OF CONTENT-TYPE:
 // application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+// Request limit
+const loginRequestLimit = rateLimit({
+   max: 10, // number of requests
+   windowMS: 15*60*1000, // 15 minutes
+   message: "Too many login requests recieved. Please wait and try again later." // message to send
+});
 
 const login = require('./handlers/login.js');
 const register = require('./handlers/register.js');
 const auth_check = require('./handlers/auth_check.js');
 //const forgotten_password = require('./handlers/forgotten_password.js');
 
-app.use('/auth/login', login);
+app.use('/auth/login', login, loginRequestLimit); // The limit will help prevent brute force attacks
 app.use('/auth/register', register);
 app.use('/auth/auth_check', auth_check);
 //app.use('/auth/forgotten_password', forgotten_password);
